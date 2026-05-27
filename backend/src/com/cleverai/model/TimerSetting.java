2315d853bc526b334ab5866ff6cd1350600d6b24
Reyyhan776 <Raihansyaabid@gmail.com>
2026-05-27 11:35:24 +0700

    Create TimerSetting.java

---
+++ backend/src/com/cleverai/model/TimerSetting.java
@@ -0,0 +1,25 @@
+package model;
+
+public class TimerSetting {
+    private int userId;
+    private int focusDuration;
+    private int shortBreak;
+    private int longBreak;
+    private boolean autoStartBreaks;
+    private boolean soundNotif;
+
+    public TimerSetting() {}
+
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
+    public int getFocusDuration() { return focusDuration; }
+    public void setFocusDuration(int focusDuration) { this.focusDuration = focusDuration; }
+    public int getShortBreak() { return shortBreak; }
+    public void setShortBreak(int shortBreak) { this.shortBreak = shortBreak; }
+    public int getLongBreak() { return longBreak; }
+    public void setLongBreak(int longBreak) { this.longBreak = longBreak; }
+    public boolean isAutoStartBreaks() { return autoStartBreaks; }
+    public void setAutoStartBreaks(boolean autoStartBreaks) { this.autoStartBreaks = autoStartBreaks; }
+    public boolean isSoundNotif() { return soundNotif; }
+    public void setSoundNotif(boolean soundNotif) { this.soundNotif = soundNotif; }
+}
