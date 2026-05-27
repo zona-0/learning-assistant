128315a104d1988912eef5149d5354fe48c79b1e
Reyyhan776 <Raihansyaabid@gmail.com>
2026-05-27 12:38:06 +0700

    Update PomodoroContoller.java

--- backend/src/com/cleverai/Contoller/PomodoroContoller.java
+++ backend/src/com/cleverai/Contoller/PomodoroContoller.java
@@ -1,13 +1,1 @@
-package controller;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.*;
-import model.TimerSetting;
-import repository.PomodoroRepository;
-import java.io.IOException;
-
-@WebServlet("/pomodoro")
-public class PomodoroServlet extends HttpServlet {
-
-}
