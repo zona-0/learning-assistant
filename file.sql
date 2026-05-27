f57346a6e689629d80e7032e0a8e53631b0ce4f3
Reyyhan776 <Raihansyaabid@gmail.com>
2026-05-27 11:27:08 +0700

    Add timer_settings and history_pomodoro tables

--- file.sql
+++ file.sql
@@ -13,6 +13,27 @@ CREATE TABLE users (
     created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Table buat nyimpen setting waktu pomodoro
+CREATE TABLE timer_settings (
+    user_id             INT PRIMARY KEY,
+    focus_duration      INT DEFAULT 25,
+    short_break         INT DEFAULT 5,
+    long_break          INT DEFAULT 15,
+    auto_start_breaks   BOOLEAN DEFAULT FALSE,
+    sound_notif         BOOLEAN DEFAULT TRUE;
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- History pomodoro
+CREATE TABLE history_pomodoro (
+    history_id    INT AUTO_INCREMENT PRIMARY KEY,
+    user_id       INT NOT NULL,
+    mode_pomo     ENUM('focus', 'short_break', 'long_break') NOT NULL,
+    waktu_mulai   DATETIME DEFAULT CURRENT_TIMESTAMP,
+    durasi_menit  INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
 INSERT INTO users (username, email, password_hash, full_name, role, is_verified)
 VALUES ('admin', 'admin@cleverai.com', SHA2('admin123', 256), 'Administrator CleverAI', 'admin', TRUE);
 
