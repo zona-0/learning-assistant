99e7f3cc05c45a42914356c51af91759d1f0d459
Raihan Abdur Rosyid <rosyidabdurraihan@gmail.com>
2026-06-02 00:55:40 +0700

    Resolve merge conflict and add PasswordChangeHandler

--- backend/src/com/cleverai/Server.java
+++ backend/src/com/cleverai/Server.java
@@ -3,11 +3,8 @@ package com.cleverai;
 import com.cleverai.handler.LoginHandler;
 import com.cleverai.handler.RegisterHandler;
 import com.cleverai.handler.ProfileHandler;
-<<<<<<< HEAD
 import com.cleverai.handler.DashboardHandler;
-=======
 import com.cleverai.handler.PasswordChangeHandler;
->>>>>>> 92f7095f9c3f49b6eb1fa80d224036a5f9109854
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
 
@@ -20,17 +17,14 @@ public class Server {
 
     public void start() throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-<<<<<<< HEAD
         server.createContext("/api/login", new LoginHandler());
         server.createContext("/api/register", new RegisterHandler());
         server.createContext("/api/profile/update", new ProfileHandler());
         server.createContext("/api/dashboard", new DashboardHandler());
-=======
-        server.createContext("/api/login",             new LoginHandler());
-        server.createContext("/api/register",          new RegisterHandler());
-        server.createContext("/api/profile/update",    new ProfileHandler());
-        server.createContext("/api/password/change",   new PasswordChangeHandler());
->>>>>>> 92f7095f9c3f49b6eb1fa80d224036a5f9109854
+        server.createContext("/api/login", new LoginHandler());
+        server.createContext("/api/register", new RegisterHandler());
+        server.createContext("/api/profile/update", new ProfileHandler());
+        server.createContext("/api/password/change", new PasswordChangeHandler());
         server.setExecutor(null);
         server.start();
         System.out.println("Server running at http://localhost:" + port);
