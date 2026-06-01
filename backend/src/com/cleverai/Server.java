ed7fd161c45c494fd6c79b32ebabaa157876f5e0
Raihan Abdur Rosyid <rosyidabdurraihan@gmail.com>
2026-06-02 01:31:07 +0700

    Refactor Server class to improve initialization
    Refactored Server class to use constructor for initialization and added start method.

--- backend/src/com/cleverai/Server.java
+++ backend/src/com/cleverai/Server.java
@@ -9,21 +9,18 @@ import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
 
 public class Server {
-    private static final int PORT = 8080;
-    
-    public static void main(String[] args) {
-        try {
-            HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
-            server.createContext("/api/login", new LoginHandler());
-            server.createContext("/api/register", new RegisterHandler());
-            server.createContext("/api/profile/update", new ProfileHandler());
-            server.createContext("/api/dashboard", new DashboardHandler());
-            server.createContext("/api/password/change", new PasswordChangeHandler());
-            server.setExecutor(null);
-            server.start();
-            System.out.println("Server is running on port " + PORT);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private HttpServer server;
+    public Server(int port) throws IOException {
+        server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/api/login", new LoginHandler());
+        server.createContext("/api/register", new RegisterHandler());
+        server.createContext("/api/profile/update", new ProfileHandler());
+        server.createContext("/api/dashboard", new DashboardHandler());
+        server.createContext("/api/password/change", new PasswordChangeHandler());
+        server.setExecutor(null); 
+    }
+    public void start() {
+        server.start();
+        System.out.println("Server is running on port " + server.getAddress().getPort());
     }
 }
