4255e9aa4b19c30f70c42dfc0bd73785e546653d
Raihan Abdur Rosyid <rosyidabdurraihan@gmail.com>
2026-06-02 01:15:08 +0700

    Refactor HistoryPomo class and remove redundant code

--- backend/src/com/cleverai/model/HistoryPomo.java
+++ backend/src/com/cleverai/model/HistoryPomo.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.cleverai.model;
 
 import java.sql.Timestamp;
@@ -10,59 +9,34 @@ public class HistoryPomo {
     private Timestamp waktuMulai;
     private int durasiMenit;
 
-    public HistoryPomo() {}
-
-    public HistoryPomo(int historyId, int userId, String modePomo, Timestamp waktuMulai, int durasiMenit) {
+    public int getHistoryId() {
+        return historyId;
+    }
+    public void setHistoryId(int historyId) {
         this.historyId = historyId;
+    }
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
         this.userId = userId;
+    }
+    public String getModePomo() {
+        return modePomo;
+    }
+    public void setModePomo(String modePomo) {
         this.modePomo = modePomo;
+    }
+    public Timestamp getWaktuMulai() {
+        return waktuMulai;
+    }
+    public void setWaktuMulai(Timestamp waktuMulai) {
         this.waktuMulai = waktuMulai;
+    }
+    public int getDurasiMenit() {
+        return durasiMenit;
+    }
+    public void setDurasiMenit(int durasiMenit) {
         this.durasiMenit = durasiMenit;
     }
-
-    public int getHistoryId() { return historyId; }
-    public int getUserId() { return userId; }
-    public String getModePomo() { return modePomo; }
-    public Timestamp getWaktuMulai() { return waktuMulai; }
-    public int getDurasiMenit() { return durasiMenit; }
-
-    public void setHistoryId(int historyId) { this.historyId = historyId; }
-    public void setUserId(int userId) { this.userId = userId; }
-    public void setModePomo(String modePomo) { this.modePomo = modePomo; }
-    public void setWaktuMulai(Timestamp waktuMulai) { this.waktuMulai = waktuMulai; }
-    public void setDurasiMenit(int durasiMenit) { this.durasiMenit = durasiMenit; }
-=======
-package com.cleverai.model;
-
-import java.sql.Timestamp;
-
-public class HistoryPomo {
-    private int historyId;
-    private int userId;
-    private String modePomo;
-    private Timestamp waktuMulai;
-    private int durasiMenit;
-
-    public HistoryPomo() {}
-
-    public HistoryPomo(int historyId, int userId, String modePomo, Timestamp waktuMulai, int durasiMenit) {
-        this.historyId = historyId;
-        this.userId = userId;
-        this.modePomo = modePomo;
-        this.waktuMulai = waktuMulai;
-        this.durasiMenit = durasiMenit;
-    }
-
-    public int getHistoryId() { return historyId; }
-    public int getUserId() { return userId; }
-    public String getModePomo() { return modePomo; }
-    public Timestamp getWaktuMulai() { return waktuMulai; }
-    public int getDurasiMenit() { return durasiMenit; }
-
-    public void setHistoryId(int historyId) { this.historyId = historyId; }
-    public void setUserId(int userId) { this.userId = userId; }
-    public void setModePomo(String modePomo) { this.modePomo = modePomo; }
-    public void setWaktuMulai(Timestamp waktuMulai) { this.waktuMulai = waktuMulai; }
-    public void setDurasiMenit(int durasiMenit) { this.durasiMenit = durasiMenit; }
->>>>>>> 92f7095f9c3f49b6eb1fa80d224036a5f9109854
 }
